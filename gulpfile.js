dc99b4db97092644d6799fe0145d7635f82e66f2
myElement88 <myelement88@gmail.com>
2018-10-12 00:03:23 +0200

    Adds example

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ var watch = require("gulp-watch");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+var rename = require("gulp-rename");
+// var nunjucks = require("gulp-nunjucks");
 
-gulp.task("default", function() {
-  console.log("Hooray, a new Gulp task.");
-});
+gulp.task("default", () =>
+  gulp
+    .src("templates/greeting.html")
+    .pipe(nunjucks.compile({ name: "myElement" }))
+    .pipe(gulp.dest("dist"))
+);
 
 gulp.task("html", function() {
   console.log("Imagine something useful done here.");
@@ -15,8 +21,9 @@ gulp.task("html", function() {
 gulp.task("css", function() {
   return gulp
     .src("./src/css/main.css")
-    .pipe(postcss([cssvars, autoprefixer]))
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(rename("css/main_bundle.css"))
+    .pipe(gulp.dest("./dist"));
 });
 
 gulp.task("watch", function() {
